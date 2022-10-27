d5ad4b36288d6e88f574f110a4c7e961377bc561
TrungVu <vutrung26072001@gmail.com>
2022-10-27 08:50:23 +0700

    bài tập thực hành CRUD 27/10

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -32,5 +32,77 @@ namespace MvcDotNet_BTH.Controllers
       }
       return View(std);
     }
+    private bool StudentExists(string id)
+    {
+      return _context.Students.Any(e=>e.StudentId == id);
+    }
+    public async Task<IActionResult> Edit(string id)
+    {
+      if(id == null)
+      {
+        return NotFound();
+      }
+      var student = await _context.Students.FindAsync(id);
+      if(student == null)
+      {
+        return NotFound();
+      } 
+      return View(student);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Edit(string id,[Bind("StudentId, StudentName")] Student std)
+    {
+      if(id != std.StudentId)
+      {
+        return NotFound();
+      }
+      if(ModelState.IsValid)
+      {
+        try
+        {
+          _context.Update(std);
+          await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if(!StudentExists(std.StudentId))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      return View(std);
+    }
+    public async Task<IActionResult> Delete(string id)
+    {
+      if(id == null)
+      {
+        return NotFound();
+      }
+
+      var std = await _context.Students.FirstOrDefaultAsync(m => m.StudentId == id);
+      if (std == null)
+      {
+        return NotFound();
+      }
+      return View(std);
+    }
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> DeleteConfirm(string id)
+    {
+      var std = await _context.Students.FindAsync(id);
+      _context.Students.Remove(std);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
   }
 }
